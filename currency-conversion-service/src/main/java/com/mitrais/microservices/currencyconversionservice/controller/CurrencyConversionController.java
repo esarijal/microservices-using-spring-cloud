0fa8d1905d6b1797e3335ce81ea7f4db12a431fc
esarijal <esa.rijal@gmail.com>
2019-03-20 11:29:19 +0700

    Basic Invoking another service using RestTemplate

--- currency-conversion-service/src/main/java/com/mitrais/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/mitrais/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,29 +1,31 @@
 package com.mitrais.microservices.currencyconversionservice.controller;
 
 import com.mitrais.microservices.currencyconversionservice.bean.CurrencyConversionBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
-
-    @Autowired
-    private Environment environment;
 
     @GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String from,
                                                   @PathVariable String to,
                                                   @PathVariable BigDecimal quantity){
-        CurrencyConversionBean bean = new CurrencyConversionBean(
-                1L, from, to, BigDecimal.valueOf(50), quantity,
-                quantity.multiply(BigDecimal.valueOf(50)),
-                Integer.parseInt(environment.getProperty("local.server.port"))
-        );
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        CurrencyConversionBean bean = new RestTemplate()
+                .getForObject("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                        CurrencyConversionBean.class, uriVariables);
+
+        bean.setQuantity(quantity);
+        bean.setTotalCalculatedAmount(quantity.multiply(bean.getConversionMultiple()));
         return bean;
     }
 }
