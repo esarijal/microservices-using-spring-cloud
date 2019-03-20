e46e8333af4dff9bdcf944bca6d9ded1d6a54cd3
esarijal <esa.rijal@gmail.com>
2019-03-20 15:57:32 +0700

    Zuul setup done, for logging only

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/mitrais/microservices/netflixzuulapigatewayserver/filter/ZuulLoggingFilter.java
@@ -0,0 +1,48 @@
+package com.mitrais.microservices.netflixzuulapigatewayserver.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+    private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     *
+     * @return priority order of the filter
+     */
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    /**
+     * we can add business logic here for returrning
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        logger.info("request -> {}, request uri -> {}", request, request.getRequestURI());
+        return null;
+    }
+}
