d8da7644cbc44933b2792b66e2ff2ac3f83300e2
esarijal <esa.rijal@gmail.com>
2019-03-20 13:59:35 +0700

    Add Feign as a REST client

--- currency-conversion-service/src/main/java/com/mitrais/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/mitrais/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,9 +1,16 @@
 package com.mitrais.microservices.currencyconversionservice.controller;
 
 import com.mitrais.microservices.currencyconversionservice.bean.CurrencyConversionBean;
+import com.mitrais.microservices.currencyconversionservice.exception.NotFoundException;
+import com.mitrais.microservices.currencyconversionservice.proxy.CurrencyExchangeServiceProxy;
+import feign.FeignException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
@@ -13,19 +20,47 @@ import java.util.Map;
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
+
     @GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String from,
                                                   @PathVariable String to,
-                                                  @PathVariable BigDecimal quantity){
+                                                  @PathVariable BigDecimal quantity) {
+
+        // Feign - Problem 1, invoking other services
         Map<String, String> uriVariables = new HashMap<>();
         uriVariables.put("from", from);
         uriVariables.put("to", to);
-        CurrencyConversionBean bean = new RestTemplate()
-                .getForObject("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-                        CurrencyConversionBean.class, uriVariables);
+        try {
+            ResponseEntity<CurrencyConversionBean> entity = new RestTemplate()
+                    .getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                            CurrencyConversionBean.class, uriVariables);
 
-        bean.setQuantity(quantity);
-        bean.setTotalCalculatedAmount(quantity.multiply(bean.getConversionMultiple()));
-        return bean;
+            CurrencyConversionBean bean = entity.getBody();
+            assert bean != null;
+            bean.setQuantity(quantity);
+            bean.setTotalCalculatedAmount(quantity.multiply(bean.getConversionMultiple()));
+            return bean;
+
+
+        } catch (HttpClientErrorException e) {
+            throw new NotFoundException("Cannot found conversion rate between " + from + " and " + to);
+        }
+    }
+
+    @GetMapping("currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from,
+                                                       @PathVariable String to,
+                                                       @PathVariable BigDecimal quantity) {
+
+        try {
+            CurrencyConversionBean bean = proxy.findExchangeValue(from, to);
+            bean.setQuantity(quantity);
+            bean.setTotalCalculatedAmount(quantity.multiply(bean.getConversionMultiple()));
+            return bean;
+        } catch (FeignException e) {
+            throw new NotFoundException("Cannot found conversion rate between " + from + " and " + to);
+        }
     }
 }
