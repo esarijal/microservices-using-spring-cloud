c83a20601c5db2665bfb75f7e8e8136fcf96f06d
esarijal <esa.rijal@gmail.com>
2019-03-20 11:14:15 +0700

    add limits-service Service that configurable from config server git

---
+++ limits-service/src/main/java/com/mitrais/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,22 @@
+package com.mitrais.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+    private int maximum;
+    private int minimum;
+
+    protected LimitConfiguration() {
+    }
+
+    public LimitConfiguration(int maximum, int minimum) {
+        this.maximum = maximum;
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+}
