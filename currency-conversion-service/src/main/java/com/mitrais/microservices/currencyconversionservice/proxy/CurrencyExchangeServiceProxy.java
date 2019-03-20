3485c61dc333dd5c723ef7ab458aba560ac7428c
esarijal <esa.rijal@gmail.com>
2019-03-20 14:16:45 +0700

    Add Ribbon as client load balancing, add simple url listofservers to register the service that ribboned

--- currency-conversion-service/src/main/java/com/mitrais/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/mitrais/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -1,11 +1,15 @@
 package com.mitrais.microservices.currencyconversionservice.proxy;
 
 import com.mitrais.microservices.currencyconversionservice.bean.CurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+// url will be put on application.properties
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyConversionBean findExchangeValue(@PathVariable("from") String from,
